2002555d94ca8800ad1bef78a726e8df925fc8cb
Nyanthie Mayang <nyanthie.mayang@student.moringaschool.com>
2022-09-06 22:25:13 +0300

    commit tree eef97f1562879d42ff438240b852052040c0c2da

--- lib/garden.rb
+++ lib/garden.rb
@@ -1,25 +1,21 @@
 # Something is missing here
 
+require_relative './plant'
 class Garden
   attr_accessor :name
 
   def initialize(name:)
     @name = name
   end
-
   def plants
     Plant.all.select do |plant|
       plant.garden == self
     end
   end
 end
-
 lawn = Garden.new(name: 'Front Lawn')
-
 basil = Plant.new(name: 'Basil')
 basil.garden = lawn
-
 cucumber = Plant.new(name: 'Cucumber')
 cucumber.garden = lawn
-
 p lawn.plants
